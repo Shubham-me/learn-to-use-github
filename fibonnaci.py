75fb0a759fe5b5fe5ad3dd31c311242bdc0bcfd1
Shubham <87982899+Shubham-me@users.noreply.github.com>
2024-05-27 23:28:40 +0530

    optimized fibonnaci.py

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,21 +1,29 @@
-def fibonacci(n):  
-      
-    # Taking 1st two fibonacci nubers as 0 and 1  
-    FibArray = [0, 1]  
-      
-    while len(FibArray) < n + 1:  
-        FibArray.append(0)  
-      
-    if n <= 1:  
-        return n  
-    else:  
-        if FibArray[n - 1] == 0:  
-            FibArray[n - 1] = fibonacci(n - 1)  
-  
-        if FibArray[n - 2] == 0:  
-            FibArray[n - 2] = fibonacci(n - 2)  
-              
-    FibArray[n] = FibArray[n - 2] + FibArray[n - 1]  
-    return FibArray[n]  
+def multiply(A,B):
+      C = [[0,0],[0,0]]
+      for _ in range(2):
+            for __ in range(2):
+                  for ___ in range(2):
+                        C[_][___] += A[_][__] * B[__][___]
+      return C
+def mat_power(A,b):
+      # return A^b
+      Res = [[1,0][0,1]]
+      while(b):
+            if(b % 2 == 1):
+                  Res = multiply(Res,A)
+            A = multiply(A,A)
+            b //= 2
+      return Res
+def fibonacci(n):
+      f0 = 0
+      f1 = 1
+      # (f2 f1) = ((1 1)(1 0)) (f1 f0)
+      # A = ((1 1)(1 0))
+      # (fn fn-1) = A^(n-1) (f1 f0)
+      # We need B = A^(n - 1)
+      A = [[1,1],
+           [1,0]]
+      B = mat_power(A,n - 1)
+      return B[0][0]
       
 print(fibonacci(9))  
